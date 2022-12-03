9734eada0fc4b05a54cd674bda8a3a6bb94f06ce
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-12-04 02:51:46 +0900

    :lipstick: 로그인페이지 UI 작업

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+import { SignInWrap } from './styled';
+
 const SignIn = () => {
-	return null;
+	const navigate = useNavigate();
+	return (
+		<SignInWrap id="login-form">
+			<img src="/image/logo.png" alt="logo" />
+			<h1>로그인</h1>
+			<div className="inputDiv">
+				<label>ID</label>
+				<input type="text" placeholder="아이디를 입력해 주세요" />
+			</div>
+			<div className="inputDiv">
+				<label>PASSWORD</label>
+				<input type="password" placeholder="비밀번호를 입력해 주세요" />
+			</div>
+			<div className="btnDiv">
+				<button className="loginBtn" type="submit">
+					로그인하기
+				</button>
+				<button className="cancleBtn" onClick={() => navigate('/')}>
+					취소
+				</button>
+			</div>
+		</SignInWrap>
+	);
 };
 
 export default SignIn;
--- src/pages/SignIn/styled.ts
+++ src/pages/SignIn/styled.ts
@@ -1,3 +1,67 @@
 import styled from 'styled-components';
 
-export const SignInWrap = styled.div``;
+export const SignInWrap = styled.form`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 1.5rem;
+	max-width: 25rem;
+	margin: 8vh auto 0;
+	padding: 3rem;
+	border: 0.2rem solid #e78111;
+	border-radius: 2rem;
+	box-shadow: 0rem 0.4rem 0.5rem 0rem #0000001a;
+	img {
+		width: 7rem;
+		height: 7rem;
+	}
+	label {
+		font-weight: 700;
+	}
+	h1 {
+		font-weight: 600;
+		font-size: 1.5rem;
+	}
+	.inputDiv {
+		display: flex;
+		flex-direction: column;
+		border-radius: 0.3rem;
+		width: 100%;
+		input {
+			padding: 0.5rem 1.5rem;
+			transition: background 0.2s ease-in-out;
+			font-size: 0.9rem;
+			&:hover,
+			&:focus {
+				background: #f1f1f1;
+			}
+		}
+	}
+	.btnDiv {
+		.loginBtn {
+			cursor: pointer;
+			padding: 0.7rem 1rem;
+			font-size: 1.3rem;
+			color: white;
+			background-color: #e78111;
+			border: 0;
+			border-radius: 0.7rem;
+			margin: 0 0.5rem;
+			&:disabled {
+				background: #e5e5e5;
+				cursor: default;
+			}
+		}
+		.cancleBtn {
+			cursor: pointer;
+			padding: 0.7rem 1.5rem;
+			font-size: 1.3rem;
+			color: white;
+			background-color: #e78111;
+			border: 0;
+			border-radius: 0.7rem;
+			margin: 0 0.5rem;
+		}
+	}
+`;
