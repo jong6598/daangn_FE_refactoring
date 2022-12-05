8c9326008562bc0c01a802f324678fb62008a238
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-12-05 22:15:51 +0900

    :sparkles: 로그인 기능 구현

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,19 +1,44 @@
 import { useNavigate } from 'react-router-dom';
-import { SignInWrap } from './styled';
+import useForm from '../../hooks/useForm';
+import validateSign from '../../utils/validateSign';
+import { signin } from '../../core/apis/auth';
+import { SignInWrap, Error } from './styled';
 
 const SignIn = () => {
 	const navigate = useNavigate();
+	const { values, errors, handleChange, handleSubmit } = useForm({
+		initialValues: { username: '', password: '' },
+		onSubmit: async (values: object) => {
+			const payload = await signin(values);
+			localStorage.setItem('TOKEN', payload.token);
+			localStorage.setItem(
+				'userInfo',
+				JSON.stringify({ userId: payload.userId, username: payload.username, nickname: payload.username }),
+			);
+			navigate('/home');
+		},
+		validateSign,
+	});
+
 	return (
-		<SignInWrap id="login-form">
+		<SignInWrap onSubmit={handleSubmit}>
 			<img src="/image/logo.png" alt="logo" />
 			<h1>로그인</h1>
 			<div className="inputDiv">
 				<label>ID</label>
-				<input type="text" placeholder="아이디를 입력해 주세요" />
+				<input type="text" name="username" value={values.username} onChange={handleChange} placeholder="아이디를 입력해 주세요" />
+				{errors.username && <Error>{errors.username}</Error>}
 			</div>
 			<div className="inputDiv">
 				<label>PASSWORD</label>
-				<input type="password" placeholder="비밀번호를 입력해 주세요" />
+				<input
+					type="password"
+					name="password"
+					value={values.password}
+					onChange={handleChange}
+					placeholder="비밀번호를 입력해 주세요"
+				/>
+				{errors.password && <Error>{errors.password}</Error>}
 			</div>
 			<div className="btnDiv">
 				<button className="loginBtn" type="submit">
