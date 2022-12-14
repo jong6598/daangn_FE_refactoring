ef6555d9c615701c02f89b6cdea7606a15a41551
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-12-14 17:29:36 +0900

    :lipstick: footer UI 수정

--- src/components/layout/Footer/styled.ts
+++ src/components/layout/Footer/styled.ts
@@ -1,22 +1,28 @@
 import styled from 'styled-components';
 
 export const FooterWrap = styled.div`
-	font-size: 1.5rem;
 	position: fixed;
+	bottom: 0;
 	display: flex;
-	justify-content: center;
+	justify-content: space-around;
 	align-items: center;
 	width: 40rem;
+	font-size: 1.5rem;
 	height: 7rem;
-	background: #ffecd7;
-	bottom: 0;
-	font-weight: 700;
-	gap: 5rem;
-	img {
-		width: 6rem;
-		height: 6rem;
-	}
-	p {
-		margin: 0;
+	background-color: #ffecd7;
+	border-top: 0.1rem solid #dedede;
+	.buttonDiv {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		button {
+			cursor: pointer;
+			border: 0;
+			background-color: transparent;
+			svg {
+				width: 2rem;
+				height: 2rem;
+			}
+		}
 	}
 `;
