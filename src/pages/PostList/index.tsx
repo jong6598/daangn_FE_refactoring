7bcb519eed3815dd4fa781faf6db917cfbe43910
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-12-17 00:57:51 +0900

    :sparkles: postList 페이지 무한스크롤을 이용한 목록 데이터 렌더링 구현

--- src/pages/PostList/index.tsx
+++ src/pages/PostList/index.tsx
@@ -1,19 +1,31 @@
-import { useState } from 'react';
-import PostListItem from '../../components/feature/PostListItem';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useInView } from 'react-intersection-observer';
 import Layout from '../../components/layout';
+import PostListItem from '../../components/feature/PostListItem';
 import DropDown from '../../components/shared/DropDown';
 import SearchInput from '../../components/shared/SearchInput';
 import { dropDownTable } from '../../constants/dropDown';
+import usePost from '../../hooks/usePost';
 import { PostListWrap } from './styled';
+import { FiPlusCircle } from 'react-icons/fi';
+import { PostDetailData } from '../../types/api';
 
 const PostList = () => {
+	const navigate = useNavigate();
+	const { ref, inView } = useInView();
 	const [searchKeyword, setSearchKeyword] = useState('');
 	const [postFilterObj, setPostFilterObj] = useState({
-		area: '',
-		category: '',
+		area: 'ALL',
+		category: 'ALL',
 	});
+	const { postListData, fetchNextPage, isFetchingNextPage } = usePost(searchKeyword, postFilterObj);
 
-	console.info(searchKeyword);
+	useEffect(() => {
+		if (inView) {
+			fetchNextPage();
+		}
+	}, [inView]);
 
 	const handleSearchByKeyword = (keyword: string) => {
 		setSearchKeyword(keyword);
@@ -37,11 +49,30 @@ const PostList = () => {
 					</div>
 					<SearchInput onSearchByKeyword={handleSearchByKeyword} />
 				</div>
-				<div className="listDiv">
-					<PostListItem />
-					<PostListItem />
-					<PostListItem />
-					<PostListItem />
+				{postListData &&
+					postListData.pages.map((page: any, idx: number) => {
+						return (
+							<React.Fragment key={idx}>
+								{page.data.map((post: PostDetailData) => (
+									<div
+										className="listDiv"
+										key={post.id}
+										style={{ cursor: 'pointer' }}
+										onClick={() => {
+											navigate(`/post/${post.id}`);
+										}}
+									>
+										<PostListItem post={post} />
+									</div>
+								))}
+							</React.Fragment>
+						);
+					})}
+				{isFetchingNextPage ? <div>로딩중...</div> : <div ref={ref} />}
+				<div className="postAddDiv">
+					<Link to="/addpost">
+						<FiPlusCircle />
+					</Link>
 				</div>
 			</PostListWrap>
 		</Layout>
