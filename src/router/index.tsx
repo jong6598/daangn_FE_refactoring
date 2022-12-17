5d536f772ac9b78b03b69951e0c97a8791823188
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-12-17 22:19:39 +0900

    :sparkles: mypage addpost editpost postdetail route path 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,12 +5,19 @@ import SignIn from '../pages/SignIn';
 import Intro from '../pages/Intro';
 import SignUp from '../pages/SignUp';
 import PostList from '../pages/PostList';
+import Mypage from '../pages/Mypage';
+import AddPost from '../pages/AddPost';
+import PostDetail from '../pages/PostDetail';
 
 const routes = [
 	{ id: 1, path: ROUTE_PATH.INTRO, element: <Intro /> },
 	{ id: 2, path: ROUTE_PATH.SIGNUP, element: <SignUp /> },
 	{ id: 3, path: ROUTE_PATH.SIGNIN, element: <SignIn /> },
 	{ id: 4, path: ROUTE_PATH.POSTLIST, element: <PostList /> },
+	{ id: 5, path: ROUTE_PATH.MYPAGE, element: <Mypage /> },
+	{ id: 6, path: ROUTE_PATH.ADDPOST, element: <AddPost /> },
+	{ id: 7, path: ROUTE_PATH.EDITPOST, element: <AddPost /> },
+	{ id: 8, path: ROUTE_PATH.POSTDETAIL, element: <PostDetail /> },
 ];
 
 const Router = () => {
--- src/router/routePath.ts
+++ src/router/routePath.ts
@@ -3,5 +3,11 @@ export const ROUTE_PATH = {
 	SIGNUP: '/signup',
 	SIGNIN: '/signin',
 	POSTLIST: '/home',
+	MYPAGE: '/mypage',
+	ADDPOST: '/addpost',
+	EDITPOST: '/post/:postId/edit',
+	POSTDETAIL: '/postdetail',
+	//FIXME: api 500에러 해결시 라우팅할 detailpage PATH
+	// POSTDETAIL: '/post/:postId',
 	NOT_FOUND: '*',
 } as const;
