71c0174ee317c62aa8036e22591d58db5d195309
jong6598 <108744804+jong6598@users.noreply.github.com>
2023-01-09 22:33:49 +0900

    :memo: Page 변수명 변경

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router';
 
-import { SignIn, Intro, SignUp, PostList, Mypage, AddPost, PostDetail } from '@src/pages';
+import { SignIn, Intro, SignUp, PostListPage, Mypage, AddPost, PostDetail } from '@src/pages';
 import { isValidArray } from '@src/utils/ArrayUtils';
 
 import { ROUTE_PATH } from './routePath';
@@ -9,7 +9,7 @@ const routes = [
 	{ id: 1, path: ROUTE_PATH.INTRO, element: <Intro /> },
 	{ id: 2, path: ROUTE_PATH.SIGNUP, element: <SignUp /> },
 	{ id: 3, path: ROUTE_PATH.SIGNIN, element: <SignIn /> },
-	{ id: 4, path: ROUTE_PATH.POSTLIST, element: <PostList /> },
+	{ id: 4, path: ROUTE_PATH.POSTLIST, element: <PostListPage /> },
 	{ id: 5, path: ROUTE_PATH.MYPAGE, element: <Mypage /> },
 	{ id: 6, path: ROUTE_PATH.ADDPOST, element: <AddPost /> },
 	{ id: 7, path: ROUTE_PATH.EDITPOST, element: <AddPost /> },
