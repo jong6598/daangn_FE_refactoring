384187b8ef63c32f35a1225d8121636d3943714e
jong6598 <108744804+jong6598@users.noreply.github.com>
2023-01-09 22:27:42 +0900

    :art: apiErrorBoundary 부분 적용을 위한 구조변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import { Suspense } from 'react';
 
-import { QueryErrorResetBoundary } from '@tanstack/react-query';
-import { useQueryErrorResetBoundary } from '@tanstack/react-query';
-
-import ApiErrorBoundary from '@src/errorBoundary/ApiErrorBoundary';
-import ApiErrorPage from '@src/errorBoundary/ApiErrorPage';
 import GlobalErrorBoundary from '@src/errorBoundary/ErrorBoundary';
 import ErrorPage from '@src/errorBoundary/GlobalErrorPage';
 import Router from '@src/router/index';
 
 const App = () => {
-	const { reset } = useQueryErrorResetBoundary();
-
 	return (
 		<GlobalErrorBoundary fallback={ErrorPage}>
-			<QueryErrorResetBoundary>
-				<Suspense fallback={<div>로딩중...</div>}>
-					<ApiErrorBoundary onReset={reset} fallback={ApiErrorPage}>
-						<Router />
-					</ApiErrorBoundary>
-				</Suspense>
-			</QueryErrorResetBoundary>
+			<Suspense fallback={<div>로딩중...</div>}>
+				<Router />
+			</Suspense>
 		</GlobalErrorBoundary>
 	);
 };
