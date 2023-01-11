8acafc976a4e56655b7ad01b257e0c64ee719b88
jong6598 <108744804+jong6598@users.noreply.github.com>
2023-01-12 04:43:51 +0900

    :bento: 실종정보란에 사용할 이미지 추가

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -3,3 +3,4 @@ export { default as Logo } from './logo.png';
 export { default as Heart } from './heart.png';
 export { default as Iphone } from './iphone.png';
 export { default as EmptyHeart } from './emptyheart.png';
+export { default as Cat } from './cat.png';
