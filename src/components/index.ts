b871ee995547fe6edc76bdb9fdeb88b0e35e6061
jong6598 <108744804+jong6598@users.noreply.github.com>
2023-01-30 22:39:33 +0900

    :memo: components import 절대경로 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,8 +3,14 @@ export { default as Footer } from '@src/components/layout/Footer';
 export { default as Layout } from '@src/components/layout';
 
 // Feature components
+export { default as MissingInfoItem } from '@src/components/feature/MissingInfoItem';
+export { default as PostList } from '@src/components/feature/PostList';
 export { default as PostListItem } from '@src/components/feature/PostListItem';
+export { default as MypageList } from '@src/components/feature/MypageList';
 
 // Shared components
 export { default as DropDown } from '@src/components/shared/DropDown';
 export { default as SearchInput } from '@src/components/shared/SearchInput';
+export { default as NoData } from '@src/components/shared/NoData';
+export { default as ToggleSwitch } from '@src/components/shared/ToggleSwitch';
+export { default as Loader } from '@src/components/shared/Loader';
